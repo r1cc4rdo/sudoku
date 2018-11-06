1474ea32391528e5986e6eb6f8f141a7655f4709
Riccardo Gherardi <gherardr@amazon.com>
2018-11-06 00:46:47 -0800

    Slim down sudoku.py

--- sudoku.py
+++ sudoku.py
@@ -1,80 +1,66 @@
+# A self-contained sudoku solver. Find me here: https://github.com/r1cc4rdo/sudoku
+
+from itertools import *
+
+sudoku = """
+
+ 8 . . | . . . | . . . 
+ . . 3 | 6 . . | . . . 
+ . 7 . | . 9 . | 2 . . 
+-------+-------+-------
+ . 5 . | . . 7 | . . . 
+ . . . | . 4 5 | 7 . . 
+ . . . | 1 . . | . 3 . 
+-------+-------+-------
+ . . 1 | . . . | . 6 8 
+ . . 8 | 5 . . | . 1 . 
+ . 9 . | . . . | 4 . . 
+
 """
-A self-contained sudoku solver.
-This is just code from the sudoku module in a single file stripped of comments and blank lines.
-For details on functions and parameters, look at board_io.py and solver_w_search.py
-"""
-from itertools import combinations, product, islice
-from collections import OrderedDict
-from copy import deepcopy
+groups = tuple([] for _ in range(27))
+for index, (r, c) in enumerate(product(range(9), repeat=2)):
+    for rcs, displacement in zip((r, c, 3 * (r / 3) + c / 3), range(0, 27, 9)):
+        groups[rcs + displacement].append(index)
 
 
-def eliminate(board):
-    for subset_size, group in product(range(1, 9), board.groups):
-        for subset in combinations((rcs for rcs in group if len(board[rcs]) == subset_size), subset_size):
-            possible_values_in_subset = reduce(lambda s, k: s | board[k], subset, set())
+def eliminate(board, groups=groups):
+    for subset_size, group in product(range(1, 9), groups):
+        for subset in combinations((index for index in group if len(board[index]) == subset_size), subset_size):
+            possible_values_in_subset = reduce(lambda s, index: s | set(board[index]), subset, set())
             if len(possible_values_in_subset) == len(subset):  # we found a constraint
-                all_supersets = [g for g in board.groups if all(rcs in g for rcs in subset)]
-                for rcs in [rcs for g in all_supersets for rcs in g if rcs not in subset]:
-                    board[rcs] -= possible_values_in_subset
-                    assert board[rcs]
+                all_supersets = [g for g in groups if all(index in g for index in subset)]
+                for index in [index for g in all_supersets for index in g if index not in subset]:
+                    board[index] = ''.join(value for value in board[index] if value not in possible_values_in_subset)
+                    assert board[index]
 
 
-def search(board):
-    lengths = [len(values) for values in board.values()]
-    pivot = board.keys()[lengths.index(min(filter(lambda x: x > 1, lengths)))]
-    for element in board[pivot]:
-        board_copy = deepcopy(board)
-        board_copy[pivot] = {element}
+def solve(board):
+    allocated, prev = 1 + 9**3, 2 + 9**3
+    while 81 < allocated < prev:  # keep eliminating
+        prev, allocated = allocated, sum(map(len, board))
+        eliminate(board)
+    if allocated == 81:  # we're done!
+        return board
+    lengths = map(len, board)  # otherwise, we need to search
+    pivot = lengths.index(min(filter(lambda x: x > 1, lengths)))
+    for board[pivot] in board[pivot]:
         try:
-            return solve(board_copy)
+            return solve(board[:])
         except AssertionError as e:
             pass  # try next element
     raise AssertionError('Search failed')
 
 
-def solve(board):
-    allocated = 1 + 9**3
-    while allocated > 81:
-        prev, allocated = allocated, sum(len(candidates) for candidates in board.itervalues())
-        if allocated == prev:  # no progress
-            return search(board)
-        eliminate(board)
-    return board
+def string_to_board(board_string, blanks='.0'):
+    return list(islice([c if c.isdigit() else '123456789' for c in board_string if c.isdigit() or c in blanks], 81))
 
 
-def string_to_board(board_representation):
-    blanks = '.x0'
-    board = OrderedDict()
-    for cnt, c in enumerate(islice([c for c in board_representation if c.isdigit() or c in blanks], 81)):
-        row, col, values = cnt // 9, cnt % 9, set([int(c)] if c not in blanks else range(1, 10))
-        board[(row, col, 3 * (row / 3) + col / 3)] = values
-    board.groups = [[rcs for rcs in board if rcs[rcs_type] == num] for num in range(9) for rcs_type in range(3)]
-    return board
-
-
-def board_to_pretty(board, multi_values=1):
-    value_strings = [''.join(str(value) for value in values) for values in board.itervalues()]
-    value_strings = [value if len(value) <= multi_values else '.' for value in value_strings]
-    width = max(len(value_string) for value_string in value_strings)
+def board_to_pretty(board):
+    width = max(map(len, board))
     sl = ' {} {} {} | {} {} {} | {} {} {} \n'  # standard line
     hd = '+'.join(['-' * (1 + (width + 1) * 3)] * 3) + '\n'  # horizontal divider
-    return (sl * 3 + hd + sl * 3 + hd + sl * 3).format(*(value_string.center(width) for value_string in value_strings))
+    return (sl * 3 + hd + sl * 3 + hd + sl * 3).format(*(values.center(width) for values in board))
 
 
 if __name__ == '__main__':
-    board_string = """
-
-     8 . . | . . . | . . . 
-     . . 3 | 6 . . | . . . 
-     . 7 . | . 9 . | 2 . . 
-    -------+-------+-------
-     . 5 . | . . 7 | . . . 
-     . . . | . 4 5 | 7 . . 
-     . . . | 1 . . | . 3 . 
-    -------+-------+-------
-     . . 1 | . . . | . 6 8 
-     . . 8 | 5 . . | . 1 . 
-     . 9 . | . . . | 4 . . 
-
-    """
-    print board_to_pretty(solve(string_to_board(board_string)))
+    print board_to_pretty(solve(string_to_board(sudoku)))
